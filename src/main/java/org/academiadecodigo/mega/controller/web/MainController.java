abcfb3f80d64ead466ebe0f70e8105a909bf57df
Romeu Rolo <romeurolopereira@101.178.37.188.rev.vodafone.pt>
2020-08-06 20:40:04 +0100

    thymeleaf implemented

--- src/main/java/org/academiadecodigo/mega/controller/web/MainController.java
+++ src/main/java/org/academiadecodigo/mega/controller/web/MainController.java
@@ -7,6 +7,7 @@ import org.academiadecodigo.mega.services.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -51,9 +52,10 @@ public class MainController {
         return "/add";
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/location")
-    public String LocationView() {
 
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public String LocationView(@PathVariable Integer id, Model model) {
+        model.addAttribute("id",id);
         return "/view";
     }
 }
